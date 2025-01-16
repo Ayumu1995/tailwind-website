1cab9158749b6fabdec284cf15ace123077bfbee
Ayumu1995 <297979@student.tamwood.com>
2025-01-15 19:42:18 -0800

    Create review section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,35 @@
 // This file is just charm, this is for using tailwind intellisence
+// You can use tailwind intellisence in your editor by adding this file
+// but this file is not necessary for tailwind to work
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#23a6f0",
+        secondary1: "#40bb15",
+        secondary2: "#3c403d",
+        backgroundGradient1: "linear-gradient(90deg, #d71489, #004788)",
+        text: "#252b42",
+        secondText: "#737373",
+        lightText: "#ffffff",
+        darkBackground: "#252b42",
+        lightBackground: "#ffffff",
+        hover: "#2a7cc7",
+        disabledElement: "#8ec2f2",
+        muted: "#bdbdbd",
+        success: "#2dc071",
+        alert: "#e77c40",
+        danger: "#e74040",
+        lightGray1: "#fafafa",
+        lightGray2: "#ececec",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      fontSize: {
+        title: "2.5em",
+      },
+    },
+  },
+};
